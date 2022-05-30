c782463c0a6abe10e4c79fbc6d544893de123bc1
jaimepy <102310187+jaimepy@users.noreply.github.com>
2022-05-30 17:29:27 +0200

    Update prueba_2.py

--- prueba_2.py
+++ prueba_2.py
@@ -1,3 +1,7 @@
 # Esto es un script de python de prueba para hacer lo mismo del ejercicio 1 desde GitKraken.
 
 print ('Esto es un script de python de prueba desde GitKraken')
+
+# Añado una rama (rama_1), y modifico el código.
+
+print ('Esto es la modificación añadida en la rama_1')
